1d95d564f6855b2cb35e17cd3520fed1f6193641
Beate Ottenwälder <b.ottenwaelder@gmail.com>
2019-06-19 21:58:19 +0200

    rewrite for fedora 30

--- bootstrap_local.sh
+++ bootstrap_local.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
-
-set -e
+#!/usr/bin/env bash
 
 echo "== BEGIN BOOTSTRAP =="
 
 hostfile=$1
 user=$2
 system=$3
+
+set -uex pipefail
 
 if [ -z "${hostfile}" ] ; then
   echo "usage: ./bootstrap_local.sh <path-to-host-file> <user> [<Fedora>]"
@@ -22,21 +22,17 @@ if [ -z "${system}" ] ; then
     system="Fedora"
 fi
 
-if [ "${system}" == "Ubuntu" ] ; then
-	echo "Ubuntu is no longer supported"
-    exit 1
-fi
-
 echo "== SETUP ${system} =="
 if [ "${system}" == "Fedora" ] ; then
 	echo "== Ensure Python on Fedora=="
-	sudo dnf -y install python2 python2-dnf
-	sudo service sshd start
+	sudo dnf -y install python3 python3-pip
+    echo "== Start ssh service on Fedora=="
+	sudo systemctl start sshd
 fi
 
 echo "== INSTALL ANSIBLE =="
-pip install markupsafe
-pip install ansible
+pip3 install markupsafe --upgrade --user
+pip3 install ansible --upgrade --user
 
 ansible-playbook -i hosts site.yml --connection=local --extra-vars "user=${user}" --ask-become-pass
 
