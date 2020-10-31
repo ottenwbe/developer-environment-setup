b00314d387cd0b26034649876793811164800e50
Beate Ottenwälder <ottenwbe@users.noreply.github.com>
2020-10-31 22:39:26 +0100

    [Feature] Support Fedora 33 (#3)

--- test/test.sh
+++ test/test.sh
@@ -10,9 +10,7 @@ pushd .
 cd ${BASEDIR}/..
 
 docker build --file=test/docker/Dockerfile.fedora --build-arg "FEDORA_VERSION=${FEDORA_VERSION}" --tag=fedora-ansible:${FEDORA_VERSION} test/docker
-docker run --detach --volume="${PWD}":/home/ansible:ro fedora-ansible:${FEDORA_VERSION} "/sbin/init" > cid
-docker exec --tty "$(cat cid)" env TERM=xterm ansible-playbook -i /home/ansible/test/docker/test_hosts /home/ansible/site.yml --connection=local --become --extra-vars '{"users": ["testuser1","testuser2"]}' --skip-tags "systemd, long_running"
-docker stop "$(cat cid)"
-docker rm "$(cat cid)"
+docker run --name=test-fedora --volume="${PWD}":/home/ansible:ro fedora-ansible:${FEDORA_VERSION} ansible-playbook -i /home/ansible/test/docker/test_hosts /home/ansible/site.yml --connection=local --become --extra-vars '{"users": ["testuser1","testuser2"]}' --skip-tags "systemd, long_running"
+docker rm test-fedora
 
 popd
