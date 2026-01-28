b9fe0cfd7a171d765d4d8e290104f852b368fa22
Beate Ottenwälder <b.ottenwaelder@gmail.com>
2026-01-28 19:22:40 +0100

    improved testing

--- bootstrap_local.sh
+++ bootstrap_local.sh
@@ -3,14 +3,16 @@
 # Ensure we are in the script directory
 cd "$(dirname "$0")"
 
-hostfile=${1:-}
-user=${2:-}
+inventoryfile=${1:-}
+extra_vars=${2:-}
 system=${3:-Fedora}
+tags=${4:-all}
+start_sshd=${5:-true}
 
 set -uex pipefail
 
-if [ -z "${hostfile}" ] || [ -z "${user}" ] ; then
-  echo "usage: ./bootstrap_local.sh <path-to-host-file> <user> [<Fedora>]"
+if [ -z "${inventoryfile}" ] || [ -z "${extra_vars}" ] ; then
+  echo "usage: ./bootstrap_local.sh <path-to-host-file> <extra-vars-json> [<Fedora> [<all|tag1,tag2> [<start_sshd>]]]"
   exit 1
 fi
 
@@ -19,8 +21,10 @@ echo "== SETUP ${system} =="
 if [ "${system}" == "Fedora" ] ; then
 	echo "== Ensure Python on Fedora=="
 	sudo dnf -y install python3 python3-pip
-  echo "== Start ssh service on Fedora=="
-	sudo systemctl start sshd
+	if [ "${start_sshd}" == "true" ]; then
+		echo "== Start ssh service on Fedora=="
+		sudo systemctl start sshd
+	fi
 fi
 
 echo "== SETUP VIRTUAL ENVIRONMENT =="
@@ -33,6 +37,6 @@ echo "== INSTALL ANSIBLE (AND PREREQUISITES)=="
 pip install --upgrade pip
 pip install -r requirements.txt
 
-ansible-playbook -i "${hostfile}" site.yml --connection=local --extra-vars "{\"users\": [{\"username\": \"${user}\"}]}" --ask-become-pass
+ansible-playbook -i "${inventoryfile}" site.yml --connection=local --extra-vars "${extra_vars}" --become --tags "${tags}"
 
 echo "== END BOOTSTRAP =="
